c7ef7214cc30c48c6f79bb6b623b3c4103e051e2
mjmoran123 <matthew.j.moran@gmail.com>
2016-09-23 09:20:50 -0500

    add raspberries to cranberries challenge to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 Challenge.create(start_id: "0LcJLqbBmaGUft1e9Mm8HV", start_name: "ABBA", end_id: "6ra4GIOgCZQZMOaUECftGN", end_name: "Frank Zappa")
 
 Challenge.create(start_id: "5M52tdBnJaKSvOpJGz8mfZ", start_name: "Black Sabbath", end_id: "0CF71zaDOJWCynIkW9bSK8", end_name: "White Zombie")
+
+Challenge.create(start_id: "7Kkx4dACo6kFSeT9wjfVA5", start_name: "The Raspberries", end_id: "7t0rwkOPGlDPEhaOcVtOt9", end_name: "The Cranberries")
